d4dff36240d50c915726163edfbb289cb58537ac
Marco Sanchez <satchito@gmail.com>
2018-08-13 18:54:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ function substract(a, b) {
   substract = a - b;
   return(substract);
 }
+
+function multiply(a, b) {
+  multiply = a * b;
+  return(multiply)
+}
+
+function divide(a, b) {
+  divide = a/b;
+  return(divide);
+}
+
+function inc(n) {
+  inc = ++n;
+  return(inc)
+}
